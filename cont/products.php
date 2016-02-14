17b3142858bacf87b2b77399094835fd5496a1f8
siobhan-walsh <siobhanemmarose@gmail.com>
2016-02-14 09:29:36 -0800

    fixed menu div error

--- cont/products.php
+++ cont/products.php
@@ -30,8 +30,7 @@ loadScripts();
 							<div class='col-sm-4 menu-price-detail'>
 								<h4 class='menu-price'>$ $price</h4>
                                 <span h4 class='menu-price add' data-pname = '$pname' data-price = '$price' data-sku = '$sku'>+ add to cart</span>
-                                <br>
-                            
+                            </div>
 						</div>
 					</div>";
             
