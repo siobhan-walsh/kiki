f2c3ca9502e69358c064f3c1142d134fdc92fc94
siobhan-walsh <siobhanemmarose@gmail.com>
2016-03-22 15:11:32 -0700

    fixing admin page

--- admin.php
+++ admin.php
@@ -1,56 +1,27 @@
 <?php
 
-// Front Controller
-require_once('./libs/PHPTAL-1.3.0/PHPTAL.php');
+    require_once('./libs/PHPTAL-1.3.0/PHPTAL.php');
 
-require_once('init.php');
-loadScripts();
+    // render the whole page using PHPTAL
+
+    // finally, create a new template object
+    $template = new PHPTAL('admin.xhtml');
+
+    // now add the variables for processing and that you created from above:
+    $template->page_title = "Admin";
 
 
-    if(!Utils::isGET()) {
-        $data = array("status" => "error", "msg" => "Only GET allowed.");
+    // execute the template
+    try {
+        echo $template->execute();
+    }
+    catch (Exception $e){
+        // not much else we can do here if the template engine barfs
+        echo $e;
     }
 
-    $parameters = new Parameters("GET");
 
-    $showUserProfileAction = new ShowUserProfileAction();
-    $showUserProfileAction->setParameters($parameters);
-    $profile = $showUserProfileAction->getProfile();
+?>
 
 
- 
-    if(Utils::isAJAX()) {
-        $data = array();
-        // AJAX means that we'll send it as JSON - at least for this call
-        if($profile == null) {
-            // didn't find a profile with that name
-            $data = array("status" => "error", "msg" => Messages::getMessages());
-        } else {
-            $data = array("status" => "success", "profile" => $profile);
-        }
-        echo json_encode($data, JSON_FORCE_OBJECT);
-        return;
 
-    } else {
-        // render the whole page using PHPTAL
-
-        // finally, create a new template object
-        $template = new PHPTAL('admin.xhtml');
-
-        // now add the variables for processing and that you created from above:
-        $template->page_title = "Index With Design Patterns";
-        $template->profile = $profile;
-
-        // messages last
-        $template->messages = Messages::getMessages();
-
-        // execute the template
-        try {
-            echo $template->execute();
-        }
-        catch (Exception $e){
-            // not much else we can do here if the template engine barfs
-            echo $e;
-        }
-
-    }
