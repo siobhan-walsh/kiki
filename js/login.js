05cf8982cf336b91b3e14b890d0f98f3169226bc
k8esak <k8esak@gmail.com>
2016-03-23 19:53:09 -0700

    admin dashboard, worked on ladmin login

--- js/login.js
+++ js/login.js
@@ -1,88 +1,54 @@
-      /*<![CDATA[*/
-      $(document).ready(function () {
+// login
 
-        // from: http://www.developerdrive.com/2013/04/turning-a-form-element-into-json-and-submiting-it-via-jquery/
-        function ConvertFormToJSON(form) {
-          var array = $(form).serializeArray();
-          var json = {};
+window.onload = function(){
 
-          jQuery.each(array, function () {
-            // don't send 'undefined'
-            json[this.name] = this.value || '';
-          });
-          return json;
-        }
+	var un = document.getElementById('un');
+	var pw = document.getElementById('pw');
+	var loginbtn = document.getElementById('subm');
 
-        function doLogin() {
-
-          var formData = ConvertFormToJSON("#loginForm");
-          //console.log("Login data to send: ", formData);
-
-          $.ajax({
-            url: "./php/login.php",
-            type: "POST",
-            dataType: "JSON",
-            data: formData,
-            success: function (returnedData) {
-              console.log("Login data returned: ", returnedData);
-
-              var status = returnedData['status'];
-              if (status == 'error') {
-                msgs = returnedData['msg'];
-                for (msg in msgs) {
-                  //console.log(msgs[msg]['text']);
-
-                  $("#AJAXMessages").html("<li class='" + msgs[msg]['type'] + "'" + ">" + msgs[msg]['text'] + "</li>");
-                }
-
-
-                /* BTW, IF LOGOUT FAILED, IT'S BECAUSE THE SESSION EXPIRED
-                   YOU COULD EASILY JUST RESET THE HTML IN THE PAGE
-                 */
-
-              } else {
-                // you're in, show profile
-                document.location.href = './adminpanel.php';
-              }
-
-
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-              console.log("AJAX Error", textStatus);
-            }
-          });
-        }
-
-        function doLogout() {
-          var formData = {
-            logout: "true"
-          };
-          //console.log("Logout data to send: ", formData);
-
-          $.ajax({
-            url: "./php/logout.php",
-            type: "POST",
-            dataType: "JSON",
-            data: formData,
-            success: function (returnedData) {
-              //console.log("Logout data returned: ", returnedData);
-              window.location.href = "index.php";
-
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-              //console.log(jqXHR.statusText, textStatus, errorThrown);
-              console.log(jqXHR.statusText, textStatus);
-            }
-          });
-        }
-
-        // login event
-        $("#loginbutton").click(doLogin);
-
-        // logout event
-        $("#logoutbutton").click(doLogout);
-
-
-
-      });
-      /*]]>*/
+	
+	loginbtn.onclick = function(){
+		document.getElementById('warn').innerHTML = '';
+		$.ajax({
+			url:"./server/login-server.php",
+			type:"POST",
+			dataType:"JSON",
+			data:{
+			
+				email:email.value,
+				pw:pw.value
+				
+				},
+			success:function(resp){
+				
+			
+				
+				if(resp.status == 'success'){
+					
+					window.location = "./dashboard.php"
+					
+				} else if(resp.status == 'fail'){
+					
+					document.getElementById('warn').innerHTML = "Sorry, wrong email or password";
+						
+				}
+				
+				
+				
+			},
+			 error: function(jqXHR, textStatus, errorThrown) {
+                        //console.log(jqXHR.statusText, textStatus, errorThrown);
+                        console.log(jqXHR.statusText, textStatus);
+                  
+			
+		
+			}
+			
+		});	
+					  
+              
+	
+		
+		
+	};
+};
---
+++ server/login-server.php
@@ -0,0 +1,102 @@
+<?php
+    // get the session
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    $methodType = $_SERVER['REQUEST_METHOD'];
+    $data = array("status" => "fail", "msg" => "$methodType");
+
+    $DBHost = "localhost";
+    $dblogin = "root";
+    $DBpassword = "root";
+    $DBname = "kiki";
+
+    $methodType = $_SERVER['REQUEST_METHOD'];
+    $data = array("status" => "fail", "msg" => "$methodType");
+
+    if ($methodType === 'POST') {
+
+
+        if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            // yes, is AJAX call
+            // answer POST call and get the data that was sent
+            if(isset($_POST["email"]) && !empty($_POST["email"])
+                && isset($_POST["pw"]) && !empty($_POST["pw"])){
+
+
+                // get the data from the post and store in variables
+                $login = $_POST["email"];
+                $password = $_POST["pw"];
+	
+                try {
+                    $conn = new PDO("mysql:host=$DBHost;dbname=$DBname", $dblogin, $DBpassword);
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    $sql = "SELECT * FROM users WHERE email = :log AND password = :pwd;";
+					
+					//"SELECT * FROM users WHERE user_name = $login AND password = $password;";
+
+//SELECT * FROM users WHERE user_name = 'mickeymouse' AND password = 'Mickey!1';
+
+                    $statement = $conn->prepare($sql);
+                    $statement->execute(array(":log" => $login, ":pwd" =>  $password));
+					$statement->execute();
+
+                    // this should be one if there's a user by that user value and password value
+                    $count = $statement->rowCount();
+				
+                    if($count > 0) {
+                        // success, so fetch the first and hopefully only record
+
+                        // http://stackoverflow.com/questions/15287905/convert-pdo-recordset-to-json-in-php
+                        // http://php.net/manual/en/pdostatement.fetchall.php
+                        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+                        $returnedLogin = $rows[0]['un'];
+					
+						
+                        $returnedPassword = $rows[0]['pw'];
+
+                      
+                        $_SESSION['un'] = $returnedLogin;
+					
+                        $_SESSION['loggedin'] = true;
+						
+
+                       
+                        $sid= session_id();
+                        $data = array("status" => "success", "sid" => $sid);
+
+
+                    } else {
+                        $data = array("status" => "fail", "msg" => "User name and/or password not correct.");
+                    }
+
+
+                } catch(PDOException $e) {
+                    $data = array("status" => "fail", "msg" => $e->getMessage());
+                }
+
+
+            } else {
+                $data = array("status" => "fail", "msg" => "Either login or password were absent.");
+            }
+
+
+
+        } else {
+            // not AJAX
+            $data = array("status" => "fail", "msg" => "Has to be an AJAX call.");
+        }
+
+
+    } else {
+        // simple error message, only taking POST requests
+        $data = array("status" => "fail", "msg" => "Error: only POST allowed.");
+    }
+
+    echo json_encode($data, JSON_FORCE_OBJECT);
+
+?>
+
